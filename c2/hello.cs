20792d74b415f6a26c3e91f3690aa9698f058bbc
Fatih Ozavci <fatih.ozavci@viproy.com>
2021-05-11 12:53:15 +1000

    Sample Codes and C2

---
+++ c2/hello.cs
@@ -0,0 +1,14 @@
+using System;
+namespace Application 
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            // It's basically System.Console.WriteLine()
+            // "using System" made it easier to use
+            Console.WriteLine("Hello, is it me you’re looking for?");
+        }
+    }
+}
+
